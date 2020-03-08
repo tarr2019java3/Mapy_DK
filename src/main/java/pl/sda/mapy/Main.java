c832a57730b38897cfd798835ca9552709e7170b
Daniel <kapron.daniel@gmail.com>
2020-03-08 10:56:21 +0100

    Generyka

--- src/main/java/pl/sda/mapy/Main.java
+++ src/main/java/pl/sda/mapy/Main.java
@@ -1,8 +1,19 @@
 package pl.sda.mapy;
 
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Generyka<String> generyka = new Generyka<String>("Test");
+
+        System.out.println(generyka.getField());
+
+
+        Generyka<Integer> generyka2 = new Generyka<Integer>(23222);
+
+        System.out.println(generyka.getField());
 
 
 
