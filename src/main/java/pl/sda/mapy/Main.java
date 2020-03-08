d3a86a68b6068474578794c4579da8608740b317
Daniel <kapron.daniel@gmail.com>
2020-03-08 11:19:57 +0100

    Mapy wstep

--- src/main/java/pl/sda/mapy/Main.java
+++ src/main/java/pl/sda/mapy/Main.java
@@ -1,5 +1,6 @@
 package pl.sda.mapy;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
@@ -7,15 +8,19 @@ public class Main {
     public static void main(String[] args) {
 
         Generyka<String> generyka = new Generyka<String>("Test");
-
-        System.out.println(generyka.getField());
-
-
+//        System.out.println(generyka.getField());
         Generyka<Integer> generyka2 = new Generyka<Integer>(23222);
+//        System.out.println(generyka2.getField());
 
-        System.out.println(generyka2.getField());
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "Jan");
+        map.put(2, "Maciej");
+        map.put(3, "Barbara");
+        map.put(4, "Kamila");
 
-
+        for (Map.Entry<Integer, String> entry: map.entrySet()){
+            System.out.println("Klucz: " + entry.getKey() + " / Wartość: " + entry.getValue());
+        }
 
     }
 
